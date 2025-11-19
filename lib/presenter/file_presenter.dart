fe88c89fcb35191052bf982552bd5dc524316a65
Rick Li <liqxrick@gmail.com>
2025-11-20 00:01:32 +0800

    fix: add favorite tab refresh support in refreshCurrent method

--- lib/presenter/file_presenter.dart
+++ lib/presenter/file_presenter.dart
@@ -799,8 +799,11 @@ class FilePresenter {
   /// 刷新当前目录
   Future<void> refreshCurrent() async {
     logger.i('FilePresenter.refreshCurrent called');
-    // 如果是最近文件模式，刷新最近文件列表
-    if (viewModel.isRecentFilesMode) {
+    // 根据当前Tab类型刷新相应内容
+    if (viewModel.currentTab == TabView.favorite) {
+      logger.d('Refreshing favorite files');
+      await loadFavoriteFiles();
+    } else if (viewModel.isRecentFilesMode) {
       logger.d('Refreshing recent files');
       await loadRecentFiles();
     } else {
